c39563540edd107eedec84da092658ade71618c9
FlutLab.io <>
2021-12-20 23:18:13 +0000

    Autosave

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText = 'You did it';
+    if (resultScore <= 8) {
+      resultText = 'your are awesome and innocent';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likeable';
+    } else if (resultScore <= 16) {
+      resultText = 'you are ... strange';
+    } else {
+      resultText = 'you are so bad';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('you did it'),
+      child: Text(resultPhrase, style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
     );
   }
 }
